5be3b358bd844f325f415174b2dad74c79341911
Jakob Staudal <jakob.staudal@lyniate.com>
2024-01-04 12:52:56 +0100

    Improve wishlist toolbar for mobile

--- src/components/data-table-toolbar.tsx
+++ src/components/data-table-toolbar.tsx
@@ -4,6 +4,7 @@ import { Table } from '@tanstack/react-table';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { DataTableViewOptions } from '@/components/data-table-view-options';
+import { useMediaQuery } from 'usehooks-ts';
 
 import { categories } from '../data/data';
 import { DataTableFacetedFilter } from './data-table-faceted-filter';
@@ -15,6 +16,7 @@ interface DataTableToolbarProps<TData> {
 
 export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>) {
 	const isFiltered = table.getState().columnFilters.length > 0;
+	const isDesktop = useMediaQuery('(min-width: 768px)');
 
 	return (
 		<div className="flex items-center justify-between">
@@ -23,10 +25,12 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
 					placeholder="Filter wishlists..."
 					value={(table.getColumn('title')?.getFilterValue() as string) ?? ''}
 					onChange={event => table.getColumn('title')?.setFilterValue(event.target.value)}
-					className="h-9 w-[150px] lg:w-[250px] font-base"
+					className="h-9 w-[150px] md:w-[250px] font-base"
 				/>
-				{table.getColumn('category') && <DataTableFacetedFilter column={table.getColumn('category')} title="Category" options={categories} />}
-				{isFiltered && (
+				{isDesktop && table.getColumn('category') && (
+					<DataTableFacetedFilter column={table.getColumn('category')} title="Category" options={categories} />
+				)}
+				{isDesktop && isFiltered && (
 					<Button variant="ghost" onClick={() => table.resetColumnFilters()} size="default" className="sm:flex hidden px-2 lg:px-3">
 						Reset
 						<Cross2Icon className="ml-2 h-4 w-4" />
@@ -35,7 +39,7 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
 			</div>
 			<div className="flex space-x-2">
 				<DataTableAddButton />
-				<DataTableViewOptions table={table} />
+				{isDesktop && <DataTableViewOptions table={table} />}
 			</div>
 		</div>
 	);
